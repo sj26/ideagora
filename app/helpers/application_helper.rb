f9b34a7d6fa87d04cecae76fb1960eae2104826a
Chris Darroch <chris@chrisdarroch.com>
2012-01-13 12:16:03 +1100

    Extract creation of the user's avatar in to a helper.
    This makes it a little easier to manage in templates, as well as ensuring consistent markup.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,8 @@ module ApplicationHelper
 	def textilize(text)
     RedCloth.new(text).to_html
   end
+  
+  def user_avatar(user, size = 50)
+    image_tag(get_gravatar(user, size), :title => user.full_name, :class => "avatar")
+  end
 end

191b9d5d3b4739291c32c700cfbb3405ee48e51c
Chris Darroch <chris@chrisdarroch.com>
2012-01-14 15:38:55 +1100

    Enable listing of the fans of a thought.

--- app/models/thought.rb
+++ app/models/thought.rb
@@ -1,6 +1,10 @@
 class Thought < ActiveRecord::Base
   has_one :ancestor, :class_name => "Thought"
-  has_many :likes
   belongs_to :user
-  has_many :fans, :through => :likes
+  has_many :likes
+  has_many :fans, :through => :likes, :source => :user
+  
+  def liked_by?(user)
+    fans.exists? user
+  end
 end

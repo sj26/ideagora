d74c6803837b6dd0880e1951553267613e314648
Elle Meredith <ellemeredith@gmail.com>
2012-01-15 10:16:42 +1100

    redirect user :back on logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.authenticate(params[:details])
     if user
       session[:user_id] = user.id
-      redirect_to root_url, :notice => 'Logged in!'
+      redirect_to :back, :notice => 'Logged in!'
     else
       flash.now.alert = 'Cannot log you in.'
       render 'new'

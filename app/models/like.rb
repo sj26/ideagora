e5b7cebee07c83c605016ccf768bfa5b3e4ab5f0
Chris Darroch <chris@chrisdarroch.com>
2012-01-14 16:02:42 +1100

    Add a scope for finding likes for a user.

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,6 @@ class Like < ActiveRecord::Base
   belongs_to :thought
   
   validates :thought_id, :uniqueness => { :scope => :user_id, :message => "You couldn't possibly like this more than you already do!" }
+  
+  scope :for_user, lambda { |user| self.where(:user_id => user.id) }
 end

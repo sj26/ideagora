0023686024879e9035b7c6e62d0625f3adcef801
Elle Meredith <ellemeredith@gmail.com>
2012-01-14 23:31:59 +1100

    a few more places to use current_camp

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,8 +2,8 @@ class EventsController < InheritedResources::Base
   before_filter :details
 
   def index
-    @in_progress = @camp.events.in_progress
-    the_rest = @camp.events.after(Time.now)
+    @in_progress = current_camp.events.in_progress
+    the_rest = current_camp.events.after(Time.now)
     @upcoming = the_rest.group_by { |a| (a.start_at + Time.zone.utc_offset + 1.hour).to_i / 6.hours }
     @upcoming = Hash[@upcoming.map { |key, val| [Time.zone.at(key * 6.hours - Time.zone.utc_offset - 1.hour), val] }]
     index!
@@ -21,21 +21,17 @@ private
   def begin_of_association_chain
     current_camp
   end
-  
-  def current_camp
-    @camp = Camp.current
-  end
 
   def details
     current_camp
     @day = best_day
-    @venues = @camp.venues
+    @venues = current_camp.venues
   end
 
   def best_day
     date = params[:day] && Date.parse(params[:day])
-    date ||= Date.today if @camp.talk_days.include?(Date.today)
-    date ||= @camp.talk_days.first
+    date ||= Date.today if current_camp.talk_days.include?(Date.today)
+    date ||= current_camp.talk_days.first
     date
   end
 end

b6ff6f4913bf2a27b73985f5791423e28645f192
Chris Darroch <chris@chrisdarroch.com>
2012-01-15 13:13:28 +1100

    Clean up markup for a project.

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -2,4 +2,8 @@ module ProjectsHelper
   def smart_description(project)
     truncate(textilize(project.try(:description)), :length => 100).try(:html_safe)
   end
+  
+  def is_owner?(project)
+    project && current_user && current_user == project.owner
+  end
 end

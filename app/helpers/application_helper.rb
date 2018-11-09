99d6266451de5ac9dd6a8fa3e9ad246020c0dbb7
Samuel Cochran <sj26@sj26.com>
2018-11-09 22:40:54 +1100

    Easier to markup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,4 @@
 module ApplicationHelper
-  def flash_helper
-    [:error, :warning, :notice, :message, :alert].collect do |key|
-      content_tag(:p, flash[key], :class => ["flash", key.to_s]) unless flash[key].blank?
-    end.join
-  end
-	
   def textilize(text)
     RedCloth.new(text).to_html
   end

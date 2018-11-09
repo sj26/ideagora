199d037c51ff4e309e0a477ed61c476c2c3f1246
Samuel Cochran <sj26@sj26.com>
2018-11-09 23:22:34 +1100

    Slightly better login
    Still terribly insecure :P

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,15 @@
 class SessionsController < ApplicationController
   def new
+    if session[:user_id]
+      redirect_to root_url, :notice => "Already logged in!"
+    end
   end
   
   def create
     user = User.authenticate(params[:details])
     if user
       session[:user_id] = user.id
-      redirect_to :back, :notice => 'Logged in!'
+      redirect_to root_url, :notice => 'Logged in!'
     else
       flash.now.alert = 'Cannot log you in.'
       render 'new'
@@ -14,7 +17,11 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-    session[:user_id] = nil  
-    redirect_to root_url, :notice => "Logged out!"
+    unless session[:user_id]
+      redirect_to root_url, :notice => "Not logged in!"
+    else
+      session.clear
+      redirect_to root_url, :notice => "Logged out!"
+    end
   end
 end

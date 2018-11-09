5a1d9443de15b4caf356e4cc2e7756925333127b
Samuel Cochran <sj26@sj26.com>
2018-11-09 22:22:07 +1100

    Make flashes not terrible

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def flash_helper
     [:error, :warning, :notice, :message, :alert].collect do |key|
-      content_tag(:p, flash[key], :class => key.to_s) unless flash[key].blank?
+      content_tag(:p, flash[key], :class => ["flash", key.to_s]) unless flash[key].blank?
     end.join
   end
 	

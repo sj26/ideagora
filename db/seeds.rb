26ea0110a8975ce18bb88be7a0c8db89f628baba
Samuel Cochran <sj26@sj26.com>
2018-11-09 19:07:38 +1100

    Some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,89 +1,76 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
+puts "Creating Camp"
+camp = Camp.find_or_create_by_name("Railscamp 24",
+  location: "Camp Woodfield, Hobart",
+  current: true,
+  time_zone: "Hobart",
+  start_at: "2018-11-23 16:00:00",
+  end_at: "2018-11-26 10:00:00",
+)
+Time.zone = camp.time_zone
+puts camp.inspect
+puts
 
-rc10 = Camp.find_by_name('Railscamp X') || Camp.create(:name => 'Railscamp X', :location => 'Woodhouse, Piccadilly, Adelaide', :current => true, :time_zone => 'Adelaide', :start_at => '2012-01-13 16:00:00', :end_at => '2012-01-16 10:00:00')
-Time.zone = rc10.time_zone
+puts "Creating organizers"
+sam = User.find_or_create_by_email("sj26@sj26.com",
+  first_name: "Samuel",
+  last_name: "Cochran",
+  twitter: "sj26",
+)
+puts sam.inspect
+attendance = sam.attendances.find_or_create_by_camp_id(camp).tap { |attendance| attendance.update_attributes(organiser: true) }
+puts attendance.inspect
+puts
 
-require 'csv'
-filename = File.join(Rails.root.to_s, 'db', 'seeds', 'rc10_attendees.csv')
-puts "Making sure we have some users for #{rc10.name}..."
-CSV.foreach(filename, :headers => true) do |row|
-  if rc10.users.find_by_email(row['email']).nil?
-    puts "Creating #{row['first_name']} #{row['last_name']}"
-    u = rc10.users.create!(
-      :first_name => row['first_name'],
-      :last_name => row['last_name'],
-      :email => row['email'],
-      :twitter => row['twitter']
-    )
-  end
-end
-puts "Done with users."
+#puts "Creating attendees"
+#require "csv"
+#filename = File.join(Rails.root.to_s, "db", "seeds", "attendees.csv")
+#CSV.foreach(filename, headers: true) do |row|
+#    puts "#{row["first_name"]} #{row["last_name"]}"
+#    user = camp.users.find_or_create_by_email(row["email"],
+#      first_name: row["first_name"],
+#      last_name: row["last_name"],
+#      twitter: row["twitter"],
+#    )
+#    puts user.inspect
+#end
+#puts
 
-%w(ben@hoskings.net
-christopher.darroch@gmail.com
-sebastian@vonconrad.com
-me@keithpitt.com).each do |email|
-  puts "Setting #{email} as organiser"
-  User.find_by_email!(email).attendances.first.update_attribute(:organiser, true)
-end
-puts 'Done with organisers.'
+puts "Creating Venues"
+main_hall = camp.venues.find_or_create_by_name("Main Hall")
+puts
 
-cd = User.find_by_email!('christopher.darroch@gmail.com')
-svc = User.find_by_email!('sebastian@vonconrad.com')
-ryanbigg = User.find_by_email!('me@ryanbigg.com')
-lachlanhardy = User.find_by_email!('lachlan@lachstock.com.au')
-
-puts 'Creating Venues'
-['Main Hall', 'Henry Rymill room', 'Safe Refuge Area (Rec Room)', 'Pem Fooks room', 'A. J. Peake room (small)', 'Old House Dining Room', 'Old House Lounge Room'].each { |v| rc10.venues.create!(:name => v) unless rc10.venues.find_by_name(v) }
-catering = Venue.find_by_name!('Main Hall')
-rec_room = Venue.find_by_name!('Safe Refuge Area (Rec Room)')
-
-puts 'Creating Events'
-events = []
-events << {:start_at => "2012-01-13 21:45:00", :end_at => "2012-01-14 03:00:00", :name => "Werewolf", :description => "Quite possibly the best game you'll ever play.", :venue => rec_room, :user => ryanbigg}
-events << {:start_at => "2012-01-13 20:30:00", :end_at => "2012-01-14 00:10:00", :name => "ScotchFTW", :description => "So much scotch.", :venue => rec_room, :user => lachlanhardy}
+puts "Creating Events"
+#events = []
+# Friday
+#events << {:start_at => "2012-01-13 21:45:00", :end_at => "2012-01-14 03:00:00", :name => "Werewolf", :description => "Quite possibly the best game you'll ever play.", :venue => rec_room, :user => ryanbigg}
+#events << {:start_at => "2012-01-13 20:30:00", :end_at => "2012-01-14 00:10:00", :name => "ScotchFTW", :description => "So much scotch.", :venue => rec_room, :user => lachlanhardy}
 # Saturday
-events << {:start_at => "2012-01-14 08:00:00", :end_at => "2012-01-14 09:00:00", :name => "Breakfast", :description => "Saturday breakfast"}
-events << {:start_at => "2012-01-14 10:30:00", :end_at => "2012-01-14 11:00:00", :name => "Morning Tea", :description => "Saturday morning tea"}
-events << {:start_at => "2012-01-14 12:30:00", :end_at => "2012-01-14 13:00:00", :name => "Lunch", :description => "Saturday lunch"}
-events << {:start_at => "2012-01-14 15:00:00", :end_at => "2012-01-14 15:30:00", :name => "Afternoon Tea", :description => "Saturday afternoon tea"}
-events << {:start_at => "2012-01-14 18:00:00", :end_at => "2012-01-14 19:00:00", :name => "Dinner", :description => "Saturday dinner"}
+#events << {:start_at => "2012-01-14 08:00:00", :end_at => "2012-01-14 09:00:00", :name => "Breakfast", :description => "Saturday breakfast"}
+#events << {:start_at => "2012-01-14 10:30:00", :end_at => "2012-01-14 11:00:00", :name => "Morning Tea", :description => "Saturday morning tea"}
+#events << {:start_at => "2012-01-14 12:30:00", :end_at => "2012-01-14 13:00:00", :name => "Lunch", :description => "Saturday lunch"}
+#events << {:start_at => "2012-01-14 15:00:00", :end_at => "2012-01-14 15:30:00", :name => "Afternoon Tea", :description => "Saturday afternoon tea"}
+#events << {:start_at => "2012-01-14 18:00:00", :end_at => "2012-01-14 19:00:00", :name => "Dinner", :description => "Saturday dinner"}
 # Sunday
-events << {:start_at => "2012-01-15 08:00:00", :end_at => "2012-01-15 09:00:00", :name => "Breakfast", :description => "Sunday breakfast"}
-events << {:start_at => "2012-01-15 10:30:00", :end_at => "2012-01-15 11:00:00", :name => "Morning Tea", :description => "Sunday morning tea"}
-events << {:start_at => "2012-01-15 12:30:00", :end_at => "2012-01-15 13:00:00", :name => "Lunch", :description => "Sunday lunch"}
-events << {:start_at => "2012-01-15 15:00:00", :end_at => "2012-01-15 15:30:00", :name => "Afternoon Tea", :description => "Sunday afternoon tea"}
-events << {:start_at => "2012-01-15 18:00:00", :end_at => "2012-01-15 19:00:00", :name => "Dinner", :description => "Sunday dinner"}
+#events << {:start_at => "2012-01-15 08:00:00", :end_at => "2012-01-15 09:00:00", :name => "Breakfast", :description => "Sunday breakfast"}
+#events << {:start_at => "2012-01-15 10:30:00", :end_at => "2012-01-15 11:00:00", :name => "Morning Tea", :description => "Sunday morning tea"}
+#events << {:start_at => "2012-01-15 12:30:00", :end_at => "2012-01-15 13:00:00", :name => "Lunch", :description => "Sunday lunch"}
+#events << {:start_at => "2012-01-15 15:00:00", :end_at => "2012-01-15 15:30:00", :name => "Afternoon Tea", :description => "Sunday afternoon tea"}
+#events << {:start_at => "2012-01-15 18:00:00", :end_at => "2012-01-15 19:00:00", :name => "Dinner", :description => "Sunday dinner"}
 # Monday
-events << {:start_at => "2012-01-16 08:00:00", :end_at => "2012-01-16 09:00:00", :name => "Breakfast", :description => "Monday breakfast. It's all over!"}
-events << {:start_at => "2012-01-16 09:30:00", :end_at => "2012-01-16 10:30:00", :name => "Game Over", :description => "RailsCamp is over. Time to pack up and head on out."}
-events << {:start_at => "2012-01-16 11:00:00", :end_at => "2012-01-16 11:05:00", :name => "Shuttle Bus to the Airport", :description => "If you need the shuttle bus to the airport, they'll be outside the Main Hall building."}
+#events << {:start_at => "2012-01-16 08:00:00", :end_at => "2012-01-16 09:00:00", :name => "Breakfast", :description => "Monday breakfast. It's all over!"}
+#events << {:start_at => "2012-01-16 09:30:00", :end_at => "2012-01-16 10:30:00", :name => "Game Over", :description => "RailsCamp is over. Time to pack up and head on out."}
+#events << {:start_at => "2012-01-16 11:00:00", :end_at => "2012-01-16 11:05:00", :name => "Shuttle Bus to the Airport", :description => "If you need the shuttle bus to the airport, they'll be outside the Main Hall building."}
+#events.each { |e| Event.create!({:camp => rc10, :venue => catering, :user => svc}.merge(e)) }
+puts
 
-events.each { |e| Event.create!({:camp => rc10, :venue => catering, :user => svc}.merge(e)) }
-puts 'Events created.'
-
-puts 'Adding a welcome message'
-content = <<-EOS
-Kickin&rsquo; in the front seat
-Sittin&rsquo; in the back seat
-Gotta make my mind up
-Which seat can I take?
-
-It's Friday, Friday
-Gotta get down on Friday
-Everybody's lookin&rsquo; forward to the weekend, weekend
-Friday, Friday
-Gettin&rsquo; down on Friday
-Everybody's lookin&rsquo; forward to the weekend
-
-Partyin&rsquo;, partyin&rsquo; (Yeah)
-Partyin&rsquo;, partyin&rsquo; (Yeah)
-Fun, fun, fun, fun
-Lookin&rsquo; forward to the weekend
-EOS
-
-Notice.create!(:title => 'Welcome to Woodhouse', :content => content, :camp => rc10, :user => cd)
-puts 'Message created.'
-puts 'Done!'
+puts "Creating Notices"
+notice = camp.notices.find_or_create_by_title("Welcome to Woodfield", content: <<TEXT, user: sam)
+🌲🌲🌲🌲🌲
+🌲🌲🌲🌲🌲
+🌲🌲🏕🌲🌲
+🌲🌲🌲🌲🌲
+🌲🌲🌲🌲🌲
+TEXT
+puts notice.inspect
+puts
+puts "All done!"

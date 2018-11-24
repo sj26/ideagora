cb51a54efd2eb1593413760c0cfe3be54b3d48d9
Samuel Cochran <sj26@sj26.com>
2018-11-24 17:16:07 +1100

    Allow switching gravatar url

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   def get_gravatar(user, size) #you can request an image size between 1px - 512px.
     email_md5 = Digest::MD5.hexdigest(user.email).to_s
-    url = "http://www.gravatar.com/avatar/" + email_md5 + ".jpg?s=" + size.to_s
+    url = "#{ENV.fetch("GRAVATAR_URL", "http://www.gravatar.com")}/avatar/" + email_md5 + ".jpg?s=" + size.to_s
   end
   
   def user_avatar(user, size = 50)

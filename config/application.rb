b9267376c4b495b5074bf3a0766c04af6ed3b462
Samuel Cochran <sj26@sj26.com>
2018-11-25 15:03:29 +1100

    This is the default now

--- config/application.rb
+++ config/application.rb
@@ -22,9 +22,6 @@ module Ideagora
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
-
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
     
